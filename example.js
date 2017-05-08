39588352b916b50b6f0b7e996616ef488143c831
Russell Steadman <russellsteadman@gmail.com>
2017-05-08 16:19:02 -0400

    Add images

--- example.js
+++ example.js
@@ -2,4 +2,15 @@ var food = require('./index.js');
 var info = food.getProductData()[100];
 console.log(info);
 console.log(JSON.stringify(food.getFoodGroup(info.group)));
-console.log(food.lookUpProduct('cracker cheezit'));
+var cheezit = food.lookUpProduct('cheezit whole wheat')[0];
+console.log(cheezit);
+var brie = food.lookUpProduct('brie')[0];
+console.log(brie);
+var briePromise = food.fetchImage(brie.id);
+briePromise.then(function (image) {
+    console.log(image);
+});
+var cheezitPromise = food.fetchImage(cheezit.id);
+cheezitPromise.then(function (image) {
+    console.log(image);
+});
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var foodWeb = function () {
+    var loadedFoodDb = null;
     this.getProductData = function () {
-        return require('./json/src/foodDb');
+        if (!loadedFoodDb) loadedFoodDb = require('./json/src/foodDb');
+        return loadedFoodDb;
     };
     this.getFoodGroup = function (groupId) {
         var foodGroup = require('./json/src/foodGroup');
@@ -14,44 +16,110 @@ var foodWeb = function () {
     this.lookUpProduct = function (search) {
         search = search.replace(/[^ \w]+/g,'').replace(/[\s]+/g,' ').toLowerCase().split(' ');
         var searchDb = require('./json/src/searchDb');
-        var matching = [];
-        for (var i in search) {
-            var curTerm = [];
-            for (var o in searchDb) {
-                if (searchDb[o].key&&searchDb[o].key.indexOf(search[i]) !== -1) curTerm.push(searchDb[o].id);
+
+        var points = {};
+
+        var give = function (to,amt) {
+            to = String(Number(to));
+            if (typeof points[to] === 'number') {
+                points[to] += amt;
+            } else {
+                points[to] = amt;
             }
-            matching.push(curTerm);
-        }
-        
-        var instance = {};
-        for (var i in matching) {
-            for (var o in matching[i]) {
-                if (typeof instance[matching[i][o]] === 'number') {
-                    instance[matching[i][o]] += 1;
-                } else {
-                    instance[matching[i][o]] = 1;
+        };
+
+        var singleFilter = function (value, index) {return this.indexOf(value) == index};
+        search = search.filter(singleFilter, search);
+
+        for (var i in search) {
+            for (var o in searchDb) {
+                if (searchDb[o].key) {
+                searchDb[o].key = searchDb[o].key.filter(singleFilter, searchDb[o].key);
+                for (var u in searchDb[o].key) {
+                    if (searchDb[o].key[u] === search[i]) {
+                        if (Number(i) === 0) {
+                            give(searchDb[o].id, 2);
+                        } else {
+                            give(searchDb[o].id, 1);
+                        }
+                        break;
+                    } else if ((searchDb[o].key[u].indexOf(search[i]) !== -1 || search[i].indexOf(searchDb[o].key[u]) !== -1) && searchDb[o].key[u].length - 2 >= search[i].length) {
+                        give(searchDb[o].id, .5);
+                        break;
+                    }
+                }
                 }
             }
         }
 
         var instanceArray = [];
-        for (var i in instance) {
-            instanceArray.push({id:i,num:instance[i]||0});
+        for (var i in points) {
+            instanceArray.push({id:i,num:points[i]||0});
         }
 
-        instanceArray.sort(function(a, b) {
+        instanceArray = instanceArray.sort(function(a, b) {
             var shifter = a.num - b.num;
-            return shifter>0?1:(shifter<0?-1:0);
+            return shifter>0?-1:(shifter<0?1:0);
         });
 
-        return this.getProductById(instanceArray[0].id);
+        for (var i in instanceArray) {
+            instanceArray[i] = this.getProductById(instanceArray[i].id);
+        }
+
+        return instanceArray;
     };
     this.getProductById = function (productId) {
-        var foodDb = this.getProductData();
-        for (var i in foodDb) {
-            if (Number(foodDb[i].id) === Number(productId)) return foodDb[i];
+        this.getProductData();
+        for (var i in loadedFoodDb) {
+            if (Number(loadedFoodDb[i].id) === Number(productId)) return loadedFoodDb[i];
         }
         return null;
+    };
+
+    var fetcher = function (query) {
+        require('es6-promise').polyfill();
+        require('isomorphic-fetch');
+        var apiurl = 'https://commons.wikimedia.org/w/api.php';
+        return fetch(apiurl+'?action=query&list=search&srsearch='+query+'&srnamespace=6&format=json')
+        .then(function(res) {
+            if (res.status >= 400) throw new Error("Bad response from server");
+            return res.json();
+        })
+        .then(function (res) {
+            if (!res.query.search[0]) throw new Error("Couldn't find a matching image.");
+            var imgtitle = encodeURIComponent(res.query.search[0].title);
+            if (!imgtitle) throw new Error("No image results.");
+            return fetch(apiurl+'?action=query&titles='+imgtitle+'&prop=imageinfo&iiprop=url|size&iiurlheight=512&format=json');
+        })
+        .then(function (res) {
+            if (res.status >= 400) throw new Error("Bad response from server");
+            return res.json();
+        })
+        .then(function (res) {
+            var page = res.query.pages[Object.keys(res.query.pages)[0]];
+            var img = page.imageinfo[0];
+            return {
+                mediaId: page.pageid,
+                image: {
+                    thumbUrl: img.thumburl,
+                    thumbWidth: img.thumbwidth,
+                    thumbHeight: img.thumbheight,
+                    url: img.url,
+                    width: img.width,
+                    height: img.height
+                }
+            }
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
+    };
+
+    this.fetchImage = function (productId) {
+        var product = this.getProductById(productId);
+        var query = encodeURIComponent(product.keywords.join(' '));
+        var groupName = this.getFoodGroup(productId);
+        fetcher(query, );
     };
 };
 
